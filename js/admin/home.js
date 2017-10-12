16350631a2b4cf18e959727fc305ce6e26e68133
johnnyzhang <1113638674@qq.com>
2017-10-12 23:40:16 +0800

    后台管理--2

--- js/admin/home.js
+++ js/admin/home.js
@@ -1,5 +1,29 @@
 /**
  * Author johnnyZhang
  * Site johnnyzhang.cn
- * CreateTime 2017/10/10.
+ * CreateTime 2017/9/12.
  */
+require(['config'], function (){
+    require(['app','zepto','app/untils'],function (app,$,untils) {
+        app.controller('home_Ctrl', ['$scope', '$rootScope', '$http', function ($scope, $rootScope, $http) {
+            // 判断是否登录，且角色是管理员
+            $http({
+                method: 'GET',
+                url: '../js/json/admin.json'
+            }).then(function successCallback(response) {
+                // 请求成功执行代码
+                $scope.admin = response.data.admin;
+            }, function errorCallback(response) {
+                // 请求失败执行代码
+                require(['sm'],function () {
+                    $.alert('Sorry,加载失败了','请重试或者待会再试');
+                });
+            });
+            $('.meun-item').click(function(){
+                if($(this).data('href')){
+                    location.href= $(this).data('href');
+                }
+            });
+        }])
+    });
+});
